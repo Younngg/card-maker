6bac0e0893b5acf27222ed42d8e129bf9a7f2fc3
Jiyeong Jeong <98656282+Younngg@users.noreply.github.com>
2022-08-08 17:28:34 +0900

    추가 : editor, preview 레이아웃

--- src/components/Maker/Maker.jsx
+++ src/components/Maker/Maker.jsx
@@ -3,6 +3,8 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import styles from './maker.module.css';
 import { useNavigate } from 'react-router-dom';
+import Editor from '../Editor/Editor';
+import Preview from '../Preview/Preview';
 
 const Maker = ({ authService }) => {
   const navigate = useNavigate();
@@ -22,6 +24,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
