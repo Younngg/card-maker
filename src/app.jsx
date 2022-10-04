79ffba6958048467297c56c15d9fa37648afdafa
Jiyeong Jeong <98656282+Younngg@users.noreply.github.com>
2022-10-04 15:38:24 +0900

    url 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styles from './app.module.css';
-import Login from './components/Login/Login';
-import Maker from './components/Maker/Maker';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./app.module.css";
+import Login from "./components/Login/Login";
+import Maker from "./components/Maker/Maker";
 
 function App({ FileInput, authService, cardRepository }) {
   return (
     <div className={styles.app}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route exact path='/' element={<Login authService={authService} />} />
+          <Route exact path="/" element={<Login authService={authService} />} />
           <Route
-            path='/maker'
-            element={<Maker FileInput={FileInput} authService={authService} cardRepository={cardRepository} />}
+            path="/maker"
+            element={
+              <Maker
+                FileInput={FileInput}
+                authService={authService}
+                cardRepository={cardRepository}
+              />
+            }
           />
         </Routes>
       </BrowserRouter>
